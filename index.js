86d535bbe95fce8e2f8aa205090b74e651d4ed42
Alexs Ruiz <ralexs.acu@gmail.com>
2025-03-19 21:44:41 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const app = express(); // instance (singleton)
+
+const fruits = []; // in memory
+
+// middleware
+app.use(express.json());
+
+app.get('/fruits', (request, response) => {
+  response.send({
+    message: 'Fruits in the storage',
+    fruits
+  });
+});
+
+app.post('/fruit', (req, res) => {
+  const { fruit } = req.body;
+  if (!!fruit) {
+    // Add validation to prevent repeated fruits
+    fruits.push(fruit);
+  }
+
+  res.send({
+    message: 'Fruit is in the storage',
+    ok: true
+  });
+});
+
+app.listen(5000, (error) => {
+  if (error) {
+    console.log({ error });
+  }
+
+  console.log('Server running on port 5000');
+});
