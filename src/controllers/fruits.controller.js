eaaa448d6324fd580b59da4f8101a138ea76ec10
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-02 21:43:26 -0600

    Implement getAllFruits logic using node:fs, node:url, and node:path

--- src/controllers/fruits.controller.js
+++ src/controllers/fruits.controller.js
@@ -1,9 +1,33 @@
-const fruits = []; // in memory
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'fruits.txt');
 
 export const getAllFruits = (_, response) => {
+  const isFruitFileCreated = fs.existsSync(filePath);
+
+  if (!isFruitFileCreated) {
+    response.send({
+      message: 'There are not fruits in the storage'
+    });
+    return;
+  }
+
+  const content = fs.readFileSync(filePath, 'utf-8');
+
+  if (content === '') {
+    response.send({
+      message: 'There are not fruits in the storage'
+    });
+    return;
+  }
+
   response.send({
-    message: 'Fruits in the storage',
-    fruits
+    message: 'Fruits in the Store',
+    fruits: content.split('\n')
   });
 };
 
