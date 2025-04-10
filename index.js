928b41b65c9f1efb38a0afa7d7cfd191f53425a3
Alexs Ruiz <ralexs.acu@gmail.com>
2025-04-09 21:16:45 -0600

    Refactor the entry point of our app

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-import express from 'express';
-import {
-  createFruit,
-  getAllFruits,
-  getFruitById
-} from './src/controllers/fruits.controller.js';
+import 'dotenv/config';
+import Server from './src/server.js';
 
-const app = express(); // instance (singleton)
-
-// middleware
-app.use(express.json());
-
-// Routes
-app.get('/fruits', getAllFruits);
-app.get('/fruit/:id', getFruitById);
-app.post('/fruit', createFruit);
-
-// listen
-app.listen(5000, (error) => {
-  if (error) {
-    console.log({ error });
-  }
-
-  console.log('Server running on port 5000');
-});
+const server = new Server();
+server.listen();
