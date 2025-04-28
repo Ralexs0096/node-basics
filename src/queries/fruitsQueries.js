e1121e0d0e3a3bfa8d27cd1e6438193c1c7f0439
Carlos M <53409854+Cema2019@users.noreply.github.com>
2025-04-28 16:49:56 -0600

    Remove queries folder. Logic will be provided in the controllers file

--- src/queries/fruitsQueries.js
+++
@@ -1,5 +0,0 @@
-
-export const getAllFruitsQuery = 'SELECT * FROM tbl_fruits';
-export const getFruitByIdQuery = 'SELECT * FROM tbl_fruits WHERE id = ?';
-export const createFruitQuery = 'INSERT INTO tbl_fruits (name, createdBy) VALUES (?, ?)';
-export const deleteFruitByIdQuery = 'DELETE FROM tbl_fruits WHERE id = ?';
